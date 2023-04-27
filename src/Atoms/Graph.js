022b97eaf6618532bde58a51684c81175ac9a4df
Harsh <harshjaiswal28555@gmail.com>
2023-04-27 12:54:36 +0530

    update

--- src/Atoms/Graph.js
+++ src/Atoms/Graph.js
@@ -5,14 +5,14 @@ const chartData = {
   labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
   datasets: [
     {
-      label: "Guest",
+      
       data: [100, 420, 130, 400, 190, 230],
       borderColor: "#9BDD7C",
       tension: 0.5,
       pointStyle: false,
+    
     },
     {
-      label: "User",
       data: [200, 390, 200, 300, 220, 440],
       borderColor: "#E9A0A0",
       tension: 0.5,
@@ -20,7 +20,22 @@ const chartData = {
     },
   ],
 };
+
+
 const chartOptions = {
+  responsive: true,
+    maintainAspectRatio: false,
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+    stacked: false,
+    plugins: {
+      title: {
+        display: true,
+        // text: "Line Chart",
+      },
+    },
   scales: {
     y: {
       min: 0,
@@ -47,8 +62,10 @@ const chartOptions = {
 };
 export default function Graph() {
   return (
-    <div style={{ height: "240px", width: "800px" }}>
+    <div style={{ width: "57rem", height: "15rem" }}>
     <Line data={chartData} options={chartOptions} />
   </div>
   );
 }
+
+
